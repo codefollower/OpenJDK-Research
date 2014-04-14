dbb5a77260b56af4da990ee4d34a125dfadf9a8f
Honghua Zhu <zhh200910@gmail.com>
2014-04-14 08:35:12 +0800

    线程测试例子

--- hotspot/my-docs/runtime/arguments.java
+++ hotspot/my-docs/runtime/arguments.java
@@ -34,8 +34,8 @@ D:\JavaSE1.8\jre\lib\resources.jar;D:\JavaSE1.8\jre\lib\rt.jar;D:\JavaSE1.8\jre\
 
 -XX:NativeMemoryTracking=detail -verbose:gc  -XX:-DumpSharedSpaces -XX:-VerifyDuringStartup -XXaltjvm=$(TargetDir) -Dsun.java.launcher=gamma -cp E:\openjdk\git\hotspot\my-test Test
 
--XX:NativeMemoryTracking=detail -verbose:gc  -XX:-DumpSharedSpaces -XX:-VerifyDuringStartup -XXaltjvm=$(TargetDir) -Dsun.java.launcher=gamma -cp E:\openjdk\java8-tutorial\src Test
-
+//加-XX:+PrintMallocFree会出现错误：Internal Error at mutex.cpp:454
+-XX:+PrintMallocFree -XX:NativeMemoryTracking=detail -verbose:gc  -XX:-DumpSharedSpaces -XX:-VerifyDuringStartup -XXaltjvm=$(TargetDir) -Dsun.java.launcher=gamma -cp E:\openjdk\git\hotspot\my-test Test
 
 加 -XX:+PrintFlagsInitial
 ==================================(在share\vm\runtime\globals.hpp定义)
