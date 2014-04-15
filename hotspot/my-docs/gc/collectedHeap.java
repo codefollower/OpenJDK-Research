acf4ea8b2d8e62bfce9f199c989053fe58ae887c
Honghua Zhu <zhh200910@gmail.com>
2014-04-15 17:07:08 +0800

    gc、CollectorPolicy

---
+++ hotspot/my-docs/gc/collectedHeap.java
@@ -0,0 +1,7 @@
+AllocatedObj
+  CHeapObj
+    CollectedHeap
+	  SharedHeap            share\vm\memory\sharedHeap.hpp
+	    G1CollectedHeap     share\vm\gc_implementation\g1\g1CollectedHeap.hpp
+		GenCollectedHeap    share\vm\memory\genCollectedHeap.hpp
+	  ParallelScavengeHeap  share\vm\gc_implementation\parallelScavenge\parallelScavengeHeap.hpp
---
+++ hotspot/my-docs/gc/概述.java
@@ -0,0 +1,11 @@
+一些缩写词
+----------------------------------------------
+DefNew                Default New (就是Serial GC)
+PSMarkSweep           Parallel Scavenge Mark Sweep
+ParNew                Parallel New
+ASParNewGeneration    Adaptive Size Parallel New Generation
+
+另请参考:
+http://hllvm.group.iteye.com/group/topic/37095
+https://blogs.oracle.com/jonthecollector/entry/our_collectors
+
